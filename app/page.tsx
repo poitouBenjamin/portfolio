554d7941c11d94fa68c079108db8e54fa35b21b3
poitouBenjamin <poitoubenjamin@gmail.com>
2026-04-17 15:54:39 +0200

    feat : add header into page

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,11 @@ import HeroSection from "@/components/HeroSection";
 import AboutMeSection from "@/components/AboutMeSection";
 import ProjectsSection from "@/components/ProjectsSection";
 import GetInTouchSection from "@/components/GetInTouchSection";
+import NavBar from "@/components/NavBar";
 export default function Home() {
   return (
     <>
+      <NavBar />
       <HeroSection />
       <AboutMeSection />
       <ProjectsSection />
