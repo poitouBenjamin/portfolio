11efe275a8547ff7a1f7df8345a1d918733b6eaa
poitouBenjamin <poitoubenjamin@gmail.com>
2026-04-17 15:45:08 +0200

    feat : add Get In Touch Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/HeroSection";
 import AboutMeSection from "@/components/AboutMeSection";
 import ProjectsSection from "@/components/ProjectsSection";
+import GetInTouchSection from "@/components/GetInTouchSection";
 export default function Home() {
   return (
     <>
       <HeroSection />
       <AboutMeSection />
       <ProjectsSection />
+      <GetInTouchSection />
     </>
   );
 }
