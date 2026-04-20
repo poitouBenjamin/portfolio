8eefdaa99674495d8188e33c1d9ea7285812ef3f
poitouBenjamin <poitoubenjamin@gmail.com>
2026-04-20 20:16:10 +0200

    skills-badge : remove skills list from card

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { ExternalLink } from "lucide-react";
 type ProjectCardProps = {
   id: number;
   title: string;
@@ -13,23 +13,14 @@ export default function ProjectCard({
   skills,
 }: ProjectCardProps) {
   return (
-    <article className="flex flex-col gap-4 w-full max-w-95 border rounded-lg">
-      <div className="flex justify-center items-center h-60 border bg-blue-900">
+    <article className="flex flex-col gap-4 w-full max-w-120 border min-h-120 h-full rounded-lg cursor-pointer hover:scale-105">
+      <div className="flex justify-center items-center h-60 border bg-blue-900 rounded-t-lg">
         {id}
       </div>
 
-      <div className="flex flex-col gap-5 px-5">
-        <h4 className="pt-5">{title}</h4>
-        <p className="w-full max-w-100">{description}</p>
-        <ul className="flex gap-2 ">
-          {skills.map((skill, id) => {
-            return (
-              <li key={id} className="border bg-gray-700 px-2 rounded-xl">
-                {skill}
-              </li>
-            );
-          })}
-        </ul>
+      <div className="flex flex-col  px-5 gap-10 justify-between items-start h-full">
+        <h4 className="pt-5 font-bold text-accent-foreground">{title}</h4>
+        <p className="w-full max-w-100 text-start text-sm">{description}</p>
       </div>
     </article>
   );
